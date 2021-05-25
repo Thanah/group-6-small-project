f9cd82b52a12b5b5382ec894468d849d12bed875
omicron1100 <michael.nehgrei@gmail.com>
2021-05-25 17:32:58 -0400

    Add example files.

---
+++ LAMPAPI/Login.py
@@ -0,0 +1,6 @@
+import mysql.connector
+
+connection = mysql.connector.connect(user = '',
+                                     password = '',
+                                     host = '',
+                                     database = 'COP4331')
